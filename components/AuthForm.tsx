6e09094b2c2ca7cbb56ac599fce42e9bace720bb
Amrit <amritsinghal2211@gmail.com>
2025-01-17 11:16:51 +0530

    database connectivity

---
+++ components/AuthForm.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { DefaultValues, FieldValues, Path, SubmitHandler ,useForm,UseFormReturn } from "react-hook-form"
+import { ZodType } from 'zod'
+import Link from 'next/link'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from './ui/button';
+import { FIELD_NAMES, FIELD_TYPES } from '@/constants'
+import ImageUpload from './ImageUpload'
+interface Props<T extends FieldValues>{
+    schema:ZodType<T>,
+    defaultValues:DefaultValues<T>,
+    onSubmit:(data:T)=>Promise<{success:boolean ,error?:string}>
+    type:"SIGN_IN"|"SIGN_UP"
+}
+const AuthForm = <T extends FieldValues> ({type,schema,defaultValues,onSubmit}:Props<T>) => {
+    const isSignIn = type === "SIGN_IN";
+    const form:UseFormReturn<T>= useForm({
+        resolver: zodResolver(schema),
+        defaultValues: defaultValues as DefaultValues<T>,
+    });
+     const handleSubmit:SubmitHandler<T> = async (data) => {
+     }
+  return (
+    <div className='flex flex-col gap-4'>
+      <h1 className='text-2xl font-semibold text-white'>
+        {isSignIn ?'Welcome Back to BookWise':"Create your library account"}
+      </h1>
+      <p className="text-light-100">
+        {isSignIn
+          ? "Access the vast collection of resources, and stay updated"
+          : "Please complete all fields and upload a valid university ID to gain access to the library"}
+      </p>
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6 w-full">
+
+    {Object.keys(defaultValues).map((field) => (
+
+      <FormField key={field}
+        control={form.control}
+        name={field as Path<T>}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className='capitalize'>{FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}</FormLabel>
+            <FormControl>
+              {field.name === 'universityCard' ? (<ImageUpload onFileChange ={field.onChange}/>):(
+              <Input
+              type={
+                FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]
+              }
+              className='form-input'
+               required  {...field} />
+              )}
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      ))}
+      <Button type="submit" className="form-btn">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </Button>
+    </form>
+    <p className="text-center text-base font-medium">
+        {isSignIn ? "New to BookWise? " : "Already have an account? "}
+
+        <Link
+          href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? "Create an account" : "Sign in"}
+        </Link>
+      </p>
+  </Form>
+  </div>
+  )
+}
+
+export default AuthForm
